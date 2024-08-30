969ac3c5cb798a3f7117cc40e72ec8229cdda054
Sunnatullox <sunnatullox82@gmail.com>
2024-08-30 20:57:53 +0500

    Add SSL certificate files

--- src/v1/utils/checkVPN.ts
+++ src/v1/utils/checkVPN.ts
@@ -7,24 +7,24 @@ const checkVPN = async (req: Request, res: Response, next: NextFunction) => {
   const ip = getClientIp(req); // Foydalanuvchining IP manzilini olish
 
   try {
-    // IP manzilini tekshirish uchun tashqi API dan foydalanish
-    const response = await axios.get(`https://ipinfo.io/${ip}/json`);
-    const { country, org } = response.data;
+    // // IP manzilini tekshirish uchun tashqi API dan foydalanish
+    // const response = await axios.get(`https://ipinfo.io/${ip}/json`);
+    // const { country, org } = response.data;
 
-    if (!org) {
-      console.log(`Organization data not available for IP: ${ip}`);
-      return next(); // Yoki xato bilan javob qaytarish mumkin
-    }
+    // if (!org) {
+    //   console.log(`Organization data not available for IP: ${ip}`);
+    //   return next(); // Yoki xato bilan javob qaytarish mumkin
+    // }
 
-    if (org.includes("VPN") || org.includes("Proxy")) {
-      return res.status(403).json({ error: "VPN is not allowed" });
-    }
+    // if (org.includes("VPN") || org.includes("Proxy")) {
+    //   return res.status(403).json({ error: "VPN is not allowed" });
+    // }
 
-    const countr = ["UZ", "RU", "KZ", "TJ", "KG"];
+    // const countr = ["UZ", "RU", "KZ", "TJ", "KG"];
 
-    if (country && countr.includes(country.toUpperCase())) {
-      return res.status(403).json({ error: "VPN is not allowed" });
-    }
+    // if (country && countr.includes(country.toUpperCase())) {
+    //   return res.status(403).json({ error: "VPN is not allowed" });
+    // }
 
     next();
   } catch (error) {
