b298d920d40841fa6cd32d472cd68e8011134531
Sunnatullox <sunnatullox82@gmail.com>
2024-08-30 21:08:26 +0500

    Add SSL certificate files

--- dist/v1/utils/checkVPN.js
+++ dist/v1/utils/checkVPN.js
@@ -12,25 +12,32 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = __importDefault(require("axios"));
 const ErrorHandler_1 = __importDefault(require("../middleware/ErrorHandler"));
 const request_ip_1 = require("request-ip");
 const checkVPN = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-    const ip = (0, request_ip_1.getClientIp)(req); // Foydalanuvchining IP manzilini olish
+    const ip = (0, request_ip_1.getClientIp)(req) ||
+        req.headers["cf-connecting-ip"] ||
+        req.headers["x-forwarded-for"] ||
+        req.headers["x-real-ip"] ||
+        req.socket.remoteAddress ||
+        req.connection.remoteAddress ||
+        "";
     try {
-        // // IP manzilini tekshirish uchun tashqi API dan foydalanish
-        // const response = await axios.get(`https://ipinfo.io/${ip}/json`);
-        // const { country, org } = response.data;
-        // if (!org) {
-        //   console.log(`Organization data not available for IP: ${ip}`);
-        //   return next(); // Yoki xato bilan javob qaytarish mumkin
-        // }
-        // if (org.includes("VPN") || org.includes("Proxy")) {
-        //   return res.status(403).json({ error: "VPN is not allowed" });
-        // }
-        // const countr = ["UZ", "RU", "KZ", "TJ", "KG"];
-        // if (country && countr.includes(country.toUpperCase())) {
-        //   return res.status(403).json({ error: "VPN is not allowed" });
-        // }
+        // IP manzilini tekshirish uchun tashqi API dan foydalanish
+        const response = yield axios_1.default.get(`https://ipinfo.io/${ip}/json`);
+        const { country, org } = response.data;
+        if (!org) {
+            console.log(`Organization data not available for IP: ${ip}`);
+            return next(); // Yoki xato bilan javob qaytarish mumkin
+        }
+        if (org && (org.includes("VPN") || org.includes("Proxy"))) {
+            return res.status(403).json({ error: "VPN is not allowed" });
+        }
+        const countr = ["UZ", "RU", "KZ", "TJ", "KG"];
+        if (country && countr.includes(country.toUpperCase())) {
+            return res.status(403).json({ error: "VPN is not allowed" });
+        }
         next();
     }
     catch (error) {
