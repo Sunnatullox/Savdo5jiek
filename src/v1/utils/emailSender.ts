a1fcc60622f5092c4cd64cff36d18c0d1fc9cb4c
Sunnatullox <sunnatullox82@gmail.com>
2024-09-08 11:46:01 +0500

    email sernder update

--- src/v1/utils/emailSender.ts
+++ src/v1/utils/emailSender.ts
@@ -8,8 +8,7 @@ export const mailSender = async (
   try {
     const transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
-      port: 465,
-      secure: true, // use SSL
+      port: 465, // use SSL
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
